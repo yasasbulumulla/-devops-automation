3c6a68d445a8bc0e4d3ed3f3167c49024128d417
yasasbulumulla <116869639+yasasbulumulla@users.noreply.github.com>
2022-11-13 21:51:54 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "welcome to javatechie-yasas";
 	}
 
 	public static void main(String[] args) {
